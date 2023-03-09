edf7a43c50f878501cd1a1372734bef1d8b11e24
stepan <jomolun@yandex.com>
2023-03-09 04:31:41 +0300

    usecase token and hash tests done

--- internal/pkg/usecase/auth_test.go
+++ internal/pkg/usecase/auth_test.go
@@ -1,6 +1,9 @@
 package usecase
 
 import (
+	"fmt"
+	mathRand "math/rand"
+	cryptoRand "crypto/rand"
 	"testing"
 	"time"
 
@@ -79,3 +82,71 @@ func TestUsecaseAuthCreateUser(t *testing.T) {
 		})
 	}
 }
+
+func TestUsecaseAuthGenerateAndCheckToken(t *testing.T) {
+
+	const iterations = 100
+
+	for i := 0; i < iterations; i++ {
+		t.Run(fmt.Sprintf("Success Token test %d", i), func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			a := mocks.NewMockAuth(c)
+			r := &repository.Repository{
+				Auth: a,
+			}
+
+			l := logMocks.NewMockLogger(c)
+			l.EXPECT().Error(gomock.Any()).AnyTimes()
+			l.EXPECT().Info(gomock.Any()).AnyTimes()
+
+			u := NewUsecase(r, l)
+
+			expectedUserID := uint(mathRand.Intn(10000))
+			expectedUserVersion := uint(mathRand.Intn(10000))
+
+			token, err := u.GenerateAccessToken(expectedUserID, expectedUserVersion)
+			assert.NoError(t, err)
+
+			gotUserID, gotUserVersion, err := u.CheckAccessToken(token)
+			assert.NoError(t, err)
+			assert.Equal(t, expectedUserID, gotUserID)
+			assert.Equal(t, expectedUserVersion, gotUserVersion)		
+		})
+	}
+}
+
+func TestUsecaseCheckHash(t *testing.T) {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+	randPass := func(n int) string {
+		b := make([]rune, n)
+		for i := range b {
+			b[i] = letterRunes[mathRand.Intn(len(letterRunes))]
+		}
+		return string(b)
+	}
+
+	randSalt := func(n int) []byte {
+		salt := make([]byte, n)
+		cryptoRand.Read(salt)
+		return salt
+	}
+
+	const iterations = 10
+	const iterationsEq = 5
+	for i := 0; i < iterations; i++ {
+		t.Run(fmt.Sprintf("Success Hash test %d", i), func(t *testing.T) {
+
+			pass := randPass(10)
+			salt := randSalt(8)
+			hash := hashPassword(pass, salt)
+
+			for j := 0; j < iterationsEq; j++ {
+				assert.Equal(t, hash, hashPassword(pass, salt))
+			}		
+		})
+	}
+}
+
