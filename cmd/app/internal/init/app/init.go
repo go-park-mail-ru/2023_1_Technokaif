62494426de9664613e7163e116a4a4b52f935e22
stepan <jomolun@yandex.com>
2023-03-21 21:44:03 +0300

    F1-2: conflicts resolved

--- cmd/app/internal/init/app/init.go
+++ cmd/app/internal/init/app/init.go
@@ -6,7 +6,7 @@ import (
 	"github.com/jmoiron/sqlx"
 
 	"github.com/go-park-mail-ru/2023_1_Technokaif/cmd/app/internal/db"
-	"github.com/go-park-mail-ru/2023_1_Technokaif/cmd/app/internal/router"
+	"github.com/go-park-mail-ru/2023_1_Technokaif/cmd/app/internal/init/router"
 
 	albumRepository "github.com/go-park-mail-ru/2023_1_Technokaif/internal/pkg/album/repository/postgresql"
 	artistRepository "github.com/go-park-mail-ru/2023_1_Technokaif/internal/pkg/artist/repository/postgresql"
