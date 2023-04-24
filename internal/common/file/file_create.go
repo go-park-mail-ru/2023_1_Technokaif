b99fc3808eb074bcdfec52d9f9e666e6ba4c1758
stepan <jomolun@yandex.com>
2023-04-24 19:01:55 +0300

    F2-1: file create fix

--- internal/common/file/file_create.go
+++ internal/common/file/file_create.go
@@ -41,7 +41,7 @@ func CreateFile(file io.ReadSeeker, extension string, dirPath string) (filename 
 		if _, err := io.Copy(newFD, file); err != nil {
 			return "", "", fmt.Errorf("can't write sent avatar to file: %w", err)
 		}
-	} else {
+	} else if err != nil {
 		return "", "", fmt.Errorf("can't check file stat: %w", err)
 	}
 
