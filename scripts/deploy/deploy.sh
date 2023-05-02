fd601e8c04dfcad5d576fa796e914aeb704da820
stepan <jomolun@yandex.com>
2023-05-02 03:52:19 +0300

    F2-2: metrics removed + deploy scripts

---
+++ scripts/deploy/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker push yarik_tri/fluire_api:latest
+docker push yarik_tri/fluire_auth:latest
+docker push yarik_tri/fluire_search:latest
+docker push yarik_tri/fluire_user:latest
