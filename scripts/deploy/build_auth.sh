4175165d7f03c4fa5fb32d7fe0feebb95ac39063
stepan <jomolun@yandex.com>
2023-05-24 00:38:46 +0300

    F3-4: pr fixes for ci

--- scripts/deploy/build_auth.sh
+++ scripts/deploy/build_auth.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #TAG="$(git branch --show-current)_$(git rev-parse --short HEAD)"
-#TAG=latest
+if [[ ! $TAG ]]; then
+    echo "Using TAG=latest"
+    TAG=latest
+fi
 
 docker build -t fluire_auth:$TAG -f Dockerfile.application --build-arg APP=auth/auth.go --build-arg PORT=4443 . 
 docker tag fluire_auth:$TAG technokaif/fluire_auth:$TAG
